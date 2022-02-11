83e11bd3b31cf529c52c0d4eecf2d93bb6f2ffad
dong3512 <dlehddls1015@naver.com>
2022-02-12 00:19:20 +0900

    .

--- app/src/main/java/com/dong/diary/DiaryApplication.java
+++ app/src/main/java/com/dong/diary/DiaryApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DiaryApplication {
-    
+
+
     public static void main(String[] args){
         SpringApplication.run(DiaryApplication.class, args);
     }
