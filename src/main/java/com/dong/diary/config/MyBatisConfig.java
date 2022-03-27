9416313c26ca0c4e759e5803255cc22b298f5e4c
dong3512 <dlehddls1015@naver.com>
2022-03-27 23:45:58 +0900

    .

--- src/main/java/com/dong/diary/config/MyBatisConfig.java
+++ src/main/java/com/dong/diary/config/MyBatisConfig.java
@@ -19,6 +19,7 @@ public class MyBatisConfig {
         sqlSessionFactory.setTypeAliasesPackage("com.dong.diary.mapper");
         return sqlSessionFactory.getObject();
     }
+
     
     @Bean
     public SqlSessionTemplate sqlSession(SqlSessionFactory sqlSessionFactory) {
